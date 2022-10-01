1a5276b3949ac6955e36daa932043792264a41be
Yasushi Sakai <yasushi.accounts@fastmail.com>
2022-10-01 05:43:54 -0400

    self address

--- mirage-city-mod/Properties/Settings.Designer.cs
+++ mirage-city-mod/Properties/Settings.Designer.cs
@@ -8,26 +8,32 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace mirage_city_mod.Properties {
-    
-    
+namespace mirage_city_mod.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
-        
+
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("18.27.127.167:9000")]
-        public string address {
-            get {
+        [global::System.Configuration.DefaultSettingValueAttribute("18.27.123.81:9000")]
+        public string address
+        {
+            get
+            {
                 return ((string)(this["address"]));
             }
         }
